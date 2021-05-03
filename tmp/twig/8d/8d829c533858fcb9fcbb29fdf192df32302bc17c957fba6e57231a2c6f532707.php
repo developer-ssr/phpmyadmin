db8f9d9c500fa5331537155e02f196dcf0ef9daf
Cris <crisjohnreytarpin@gmail.com>
2021-05-03 13:11:42 +0800

    files

---
+++ tmp/twig/8d/8d829c533858fcb9fcbb29fdf192df32302bc17c957fba6e57231a2c6f532707.php
@@ -0,0 +1,64 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/results/empty_display.twig */
+class __TwigTemplate_61eda46f0f75058af0d6556c1572d1b13572d66df87158fc60687582c4e49f3d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<td ";
+        echo twig_escape_filter($this->env, ($context["align"] ?? null), "html", null, true);
+        echo " class=\"";
+        echo twig_escape_filter($this->env, ($context["classes"] ?? null), "html", null, true);
+        echo "\"></td>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/results/empty_display.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/results/empty_display.twig", "/home/vagrant/code/phpmyadmin/templates/display/results/empty_display.twig");
+    }
+}
